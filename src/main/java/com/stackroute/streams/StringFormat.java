fc9e19d386ac6248f0df6ef6e43ca3c32f410c78
shahid <shahdbest@gmail.com>
2023-04-18 14:15:37 +0530

    question

---
+++ src/main/java/com/stackroute/streams/StringFormat.java
@@ -0,0 +1,11 @@
+package com.stackroute.streams;
+
+import java.util.List;
+
+public class StringFormat {
+
+    //write logic to find the format for given list and return result
+    public String findStringFormat(List<Integer> input) {
+        return null;
+    }
+}
